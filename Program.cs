4b90fb1ac51a699c0771402ec26adc45c6a310f8
Alanoud <Alanoud2022it@gmail.com>
2024-09-23 10:03:46 +0400

    connection to the repo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+        }
+    }
+}
